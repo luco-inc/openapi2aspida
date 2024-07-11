f3538545fa92dbd5a4bc4bba34f532cbf79a60ad
NEKOYASAN <hello@nekoyasan.me>
2024-07-11 17:58:23 +0900

    rimraf support to node20

--- samples/rimraf.js
+++ samples/rimraf.js
@@ -1,9 +1,12 @@
 // for $ npm run dev:openapi
 require('fs')
   .readdirSync(__dirname)
-  .filter(n => require('fs').statSync(`${__dirname}/${n}`).isDirectory() && n !== 'docs.baikalplatform.com')
-  .forEach(n =>
-    require('fs').rmdir(`${__dirname}/${n}`, { recursive: true }, err => {
-      if (err) console.log(err)
-    })
+  .filter(
+    n =>
+      require('fs').statSync(`${__dirname}/${n}`).isDirectory() && n !== 'docs.baikalplatform.com',
   )
+  .forEach(n =>
+    require('fs').rmSync(`${__dirname}/${n}`, { recursive: true }, err => {
+      if (err) console.log(err);
+    }),
+  );
